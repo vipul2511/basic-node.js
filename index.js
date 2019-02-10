eced3008f803a4c58548113037a072bf40df89c8
vipul2511 <vshrimali709@gmail.com>
2019-02-10 21:49:17 +0530

    this is the basic node js program

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect={
+    perimeter: (a,b) =>(2*(a+b)),
+    area: (x,y) => (x*y)
+};
+
+function answer(l,b) {
+    console.log("perimeter for rectangle with l="+l+"and b="+b);
+    if(l<=0 || b<=0){
+        console.log("rectangle dimension should be with l= "+l +"and  b="+b);
+    }
+    else{
+        console.log("the area of rectangle with l="+rect.area(l,b));
+        console.log("the perimeter of rectangle is "+rect.perimeter(l,b));
+    }
+}
+
+answer(2,6);
+answer(4,7);
+answer(0,3);
+answer(-1,7);
